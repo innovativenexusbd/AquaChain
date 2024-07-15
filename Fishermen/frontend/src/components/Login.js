f53986abd05a436c952cfab8d4993f600c2cef34
Abid Ehsan Digonto <mm.mindmoves@gmail.com>
2024-07-15 13:55:58 +0600

    Create Login.js

---
+++ Fishermen/frontend/src/components/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import authService from '../services/authService';
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        try {
+            await authService.login(formData);
+            alert('Login successful!');
+        } catch (err) {
+            alert('Error: ' + err.response.data.msg);
+        }
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label>Email</label>
+                <input type="email" name="email" value={email} onChange={onChange} required />
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" name="password" value={password} onChange={onChange} required />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default Login;
