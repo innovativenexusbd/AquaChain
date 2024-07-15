b004deea4b62ff2972574aa11ac2517612990d91
Abid Ehsan Digonto <mm.mindmoves@gmail.com>
2024-07-15 13:42:43 +0600

    Update authController.js

--- Fishermen/backend/controllers/authController.js
+++ Fishermen/backend/controllers/authController.js
@@ -1,44 +1,3 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-exports.register = async (req, res) => {
-    const { name, email, password, identityDocument } = req.body;
-
-    try {
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ msg: 'User already exists' });
-        }
-
-        user = new User({ name, email, password, identityDocument });
-
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-
-        await user.save();
-
-        const payload = {
-            user: {
-                id: user.id,
-            },
-        };
-
-        jwt.sign(
-            payload,
-            process.env.JWT_SECRET,
-            { expiresIn: 360000 },
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token });
-            }
-        );
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-};
-
 exports.login = async (req, res) => {
     const { email, password } = req.body;
 
@@ -56,6 +15,7 @@ exports.login = async (req, res) => {
         const payload = {
             user: {
                 id: user.id,
+                approved: user.approved
             },
         };
 
@@ -65,7 +25,7 @@ exports.login = async (req, res) => {
             { expiresIn: 360000 },
             (err, token) => {
                 if (err) throw err;
-                res.json({ token });
+                res.json({ token, approved: user.approved });
             }
         );
     } catch (err) {
