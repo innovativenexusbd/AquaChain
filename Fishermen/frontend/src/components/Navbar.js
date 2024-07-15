856ab33e1b5ab0e63c1fd043165e0c32a4f483c9
Abid Ehsan Digonto <mm.mindmoves@gmail.com>
2024-07-15 13:53:51 +0600

    Create Navbar.js

---
+++ Fishermen/frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>AquaChain</h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
